afdf76bed1faab39268b21b455cc58f57cf9709e
moderncoding101 <104559521+moderncoding101@users.noreply.github.com>
2022-04-28 15:31:16 +0200

    init

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-        function string_check($input)
-        {
-            #echo "<pre>"; // Enables display of line feeds
-            $solution = file_get_contents("strings_solution.txt");
-            #echo "</pre>"; // Terminates pre tag
-            return ($input==$solution) ? true : false;
-        }
